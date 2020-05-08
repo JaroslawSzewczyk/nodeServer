651396771450c0093b41a3b3580912da99a39a73
Jarosław Szewczyk <jaroslaw.szewczyk@tutanota.com>
2020-05-08 16:00:48 +0100

    Add form

--- server.js
+++ server.js
@@ -1,13 +1,18 @@
 const express = require('express');
 const path = require('path');
 const hbs = require('express-handlebars');
+const multer = require('multer');
+const upload = multer();
 
 const app = express();
 
 app.engine('.hbs', hbs());
 app.set('view engine', '.hbs');
 
-app.use("/public",express.static(path.join(__dirname, 'public/')));
+app.use("/public", express.static(path.join(__dirname, 'public/')));
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
 app.get('/', (req, res) => {
   res.render('index');
@@ -19,6 +24,19 @@ app.get('/about', (req, res) => {
 
 app.get('/contact', (req, res) => {
   res.render('contact');
+});
+
+app.post('/contact/send-message', upload.single('projectFile'), (req, res) => {
+
+  const { author, sender, title, message } = req.body;
+
+  if (author && sender && title && message && req.file) {
+    res.render('contact', { isSent: true, fileName: req.file.originalname });
+  }
+  else {
+    res.render('contact', { isError: true })
+  }
+
 });
 
 app.get('/info', (req, res) => {
